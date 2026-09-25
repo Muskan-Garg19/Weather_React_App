7c9d7d37045db129e74ea6565d1c75e26615600b
Muskan_Garg.19 <im.muskangarg@gmail.com>
2026-09-25 11:53:43 +0530

    Fix Render allowed host

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,7 @@ import { defineConfig } from 'vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  preview: {
+    allowedHosts: ['weather-react-app-rj31.onrender.com']
+  }
 })
